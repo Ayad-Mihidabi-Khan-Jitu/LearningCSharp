e509b62fd723a776196afae3085d6e48e17e813a
Ayad Mihidabi Khan Jitu <jitu15@cse.pstu.ac.bd>
2020-08-25 03:06:48 +0600

    rat

--- Constructor/ImplicitConstructor.cs
+++ Constructor/ImplicitConstructor.cs
@@ -4,73 +4,59 @@ namespace Constructor
 {
     public class ImplecitConstructor
     {
-        int firstNum, SecondNum;
-        bool ControllSwitch;
-        string firstName;
+        string name;
+        int battarybackup;
+        bool controllable;
+        string feature;
         
-        ImplecitConstructor(){
-        firstNum=0; 
-        SecondNum=0;
-        ControllSwitch=false;
-        firstName=null;    
-        } 
-        
-
         /*
-        ImplecitConstructor(int f, int s, bool c, string fn)
-            {//f=12,s=13,c=true,fn="SAIKAT"
-            firstNum = f; //12
-            SecondNum = s; //13
-            ControllSwitch = c; //true
-            firstName = fn; //"SAIKAT"
+        ImplecitConstructor(){
+            string name;
+            int battarybackup;
+            bool controllable;
+            string feature;
             }
         */
-            public static void Main(string[] args)
+
+        
+        ImplecitConstructor(string s, string f, bool t, int n)
+            {
+             string name = s;
+             int battarybackup = n;
+             bool controllable = t;
+             string feature = f;
+            }
+        
+        public static void Main(string[] args)
         {
-            //ImplecitConstructor valuePrint = new ImplecitConstructor();
-            //Constructor.ImplecitConstructor valuePrint = new Constructor.ImplecitConstructor(); //valid
-            // ImplecitConstructor valuePrint = new Constructor.ImplecitConstructor(); //also valid
+            //ImplecitConstructor robot = new ImplecitConstructor();
+            //Constructor.ImplecitConstructor tom = new Constructor.ImplecitConstructor(); //valid
+            // ImplecitConstructor tom = new Constructor.ImplecitConstructor(); //also valid
             /*
-            Console.WriteLine(valuePrint.firstName);
-            Console.WriteLine(valuePrint.SecondNum);
-            Console.WriteLine(valuePrint.firstNum);
-            Console.WriteLine(valuePrint.ControllSwitch);
+            Console.WriteLine("This is "+robot.name);
+            Console.WriteLine(robot.feature);
+            Console.WriteLine("This is kotha shone "+robot.controllable);
+            Console.WriteLine("Battary backup "+robot.battarybackup+"hours");
             */
+
             /*
-            valuePrint.firstName = "saikat";
-            valuePrint.firstNum = 1;
-            valuePrint.SecondNum = 2;
-            valuePrint.ControllSwitch = true;
+            robot.name = "Tom";
+            robot.feature = "He takes care of Jerry";
+            robot.controllable = true;
+            robot.battarybackup = 12;
             
-            Console.WriteLine(valuePrint.firstName);
-            Console.WriteLine(valuePrint.SecondNum);
-            Console.WriteLine(valuePrint.firstNum);
-            Console.WriteLine(valuePrint.ControllSwitch);
+            Console.WriteLine("This is "+robot.name);
+            Console.WriteLine(robot.feature);
+            Console.WriteLine("This is kotha shone "+robot.controllable);
+            Console.WriteLine("Battary backup "+robot.battarybackup+"hours");
 */
-            
-            ImplecitConstructor valuePrint = new ImplecitConstructor(12,13,true,"SAIKAT");
-            Console.WriteLine(valuePrint.firstName);
-            Console.WriteLine(valuePrint.SecondNum);
-            Console.WriteLine(valuePrint.firstNum);
-            Console.WriteLine(valuePrint.ControllSwitch);
 
-            valuePrint.firstName = "Eshita";
-            valuePrint.firstNum = 111;
-            valuePrint.SecondNum = 122;
-            valuePrint.ControllSwitch = false;
+            ImplecitConstructor robot = new ImplecitConstructor("TOm", "He takes care of Jerry", true,12);
+            Console.WriteLine("This is "+robot.name);
+            Console.WriteLine(robot.feature);
+            Console.WriteLine("This is kotha shone "+robot.controllable);
+            Console.WriteLine("Battary backup "+robot.battarybackup+"hours");
 
-            Console.WriteLine(valuePrint.firstName);
-            Console.WriteLine(valuePrint.SecondNum);
-            Console.WriteLine(valuePrint.firstNum);
-            Console.WriteLine(valuePrint.ControllSwitch);
-
-            ImplecitConstructor valuePrint1 = new ImplecitConstructor(21, 22, true, "JITU");
-            Console.WriteLine(valuePrint1.firstName);
-            Console.WriteLine(valuePrint1.SecondNum);
-            Console.WriteLine(valuePrint1.firstNum);
-            Console.WriteLine(valuePrint1.ControllSwitch);
-            */
-            
             }
 
     }
